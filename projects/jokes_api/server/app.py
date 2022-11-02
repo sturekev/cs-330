093101076b90c72d36aeced99dc46d5a9d0b6ab6
Phat Tu <77705393+sturekev@users.noreply.github.com>
2022-11-02 03:25:13 -0500

    project jokes_api, app.py, handle error left

--- projects/jokes_api/server/app.py
+++ projects/jokes_api/server/app.py
@@ -4,41 +4,48 @@ jokes api
 """
 #!/usr/bin/env python3
 """Flask application to use `pyjokes`"""
-
 import random
-from re import I
-from typing import List
-
 import pyjokes
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, jsonify
+# get lấy cái j đấy từ server về 
+# post muốn đưa lên server một cái j đấy 
+# tạo một cái object để db server lưu 
+# muốn server trả về cái j đấy đó là get 
 app = Flask(__name__)
 
-def getJokes (languages: str,category: str,limit: int) -> list:
-    return pyjokes.get_jokes(language="de", category="neutral")[::10] 
+def getJokes (language: str,category: str,limit = None) -> list:
+    try :
+        if limit == None:
+            data = pyjokes.get_jokes(language, category)
+        else:
+            data = pyjokes.get_jokes(language, category)[:limit]
+    except pyjokes.pyjokes.CategoryNotFoundError :
+        data = ["No kidding!"]
+    return data 
+
 @app.route("/", methods=["GET"])
 def index():
     """Render the template with form"""
     return render_template("base.html")
 
+@app.route("/api/v1/jokes", methods=["GET"])
+def sendV1Jokes01():
+    category = request.args.get('category')
+    language = request.args.get('language')
+    limit = request.args.get('number')
+    id = request.args.get('id')
+    data = getJokes(category=category, language=language, limit=limit)
+    if id == None:
+        data = getJokes(category=category, language=language, limit=limit)
+    else:
+        data = getJokes(category=category, language=language)[id]
+    return jsonify(data)
 
-@app.route("/", methods=["POST"])
-def index_jokes():
-    """Render the template with jokes"""
-    if "category" in request.form and "language" in request.form:
-        result = send_joke(language=request.form.get("language"), category=request.form.get("category"),number=int(request.form.get("number")))
-        print(result)
-        return render_template("jokes.html", result = result)
-    else :
-        raise "No data to process"
+@app.route("/api/v1/jokes/<str:language>/<str:category>/<int:limit>/<int:id>", methods=["GET"])
+def sendV1Jokes02(language,category,limit = None, id = None):
+    if id:
+        data = getJokes(language=language,category=category,limit=limit)
+    else:
+        data = getJokes(language=language,category=category)[id]
+    return jsonify(data)
 
-def send_joke(
-    language: str = "en", category: str = "all", number: int = 1
-) -> List[str]:
-    """Return a list of jokes"""
-    try :
-        result = pyjokes.get_jokes(language, category)[:number]
-
-    except pyjokes.pyjokes.CategoryNotFoundError :
-        result = ["No kidding!"]
-    return result
