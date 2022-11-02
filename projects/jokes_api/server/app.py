77d55323f9c807e8955f9d6253b285c7745d0334
sturekev <tuphatlqd@gmail.com>
2022-11-02 02:33:24 -0500

    dang lam

--- projects/jokes_api/server/app.py
+++ projects/jokes_api/server/app.py
@@ -2,3 +2,43 @@
 """
 jokes api
 """
+#!/usr/bin/env python3
+"""Flask application to use `pyjokes`"""
+
+import random
+from re import I
+from typing import List
+
+import pyjokes
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+def getJokes (languages: str,category: str,limit: int) -> list:
+    return pyjokes.get_jokes(language="de", category="neutral")[::10] 
+@app.route("/", methods=["GET"])
+def index():
+    """Render the template with form"""
+    return render_template("base.html")
+
+
+@app.route("/", methods=["POST"])
+def index_jokes():
+    """Render the template with jokes"""
+    if "category" in request.form and "language" in request.form:
+        result = send_joke(language=request.form.get("language"), category=request.form.get("category"),number=int(request.form.get("number")))
+        print(result)
+        return render_template("jokes.html", result = result)
+    else :
+        raise "No data to process"
+
+def send_joke(
+    language: str = "en", category: str = "all", number: int = 1
+) -> List[str]:
+    """Return a list of jokes"""
+    try :
+        result = pyjokes.get_jokes(language, category)[:number]
+
+    except pyjokes.pyjokes.CategoryNotFoundError :
+        result = ["No kidding!"]
+    return result
