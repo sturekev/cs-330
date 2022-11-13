5314cfe81a8d2de7d9ab1ec5d44c9bd81770a49a
Phat Tu <77705393+sturekev@users.noreply.github.com>
2022-11-13 15:32:24 -0600

    geo done

--- exercises/geo/app.py
+++ exercises/geo/app.py
@@ -1,33 +1,90 @@
-import requests
 from flask import Flask, request, render_template, send_from_directory
 from flask import redirect, url_for
-import records
-
+import sqlite3
 app = Flask(__name__)
 
+THE_WORLD = []
+CACHE = {}
 
 def get_data_from_db(query: str) -> list:
     """retrieve data from the database and return to the user"""
-    raise NotImplementedError
+    response = set()
+    connection = sqlite3.connect('world.sqlite3')
+    cur = connection.cursor()
+    table = query
+    if table:
+        cur.execute(query)
+    else :
+        return response.append({'Error': "table name not in database"})
+    for i in cur:
+        response.add    (i[0])
+    print (response)   
+    cur.close() 
+    return list(response)
+# get_data_from_db(query="dds") 
+def get_country_info(column, value):
+    response = []
+    connection = sqlite3.connect('world.sqlite3')
+    cur = connection.cursor()
+    cur.execute(f"select * from country where {column} = ?", (value,))
+    for AFG, name, continent, region, surfacearea, indepyear, population, lifeexpectancy, gnp, gnpold, localname, governmentform, headofstate, capital, code2 in cur:
+        response.append([AFG,name, continent, region, surfacearea, indepyear, population, lifeexpectancy, gnp, gnpold, localname, governmentform, headofstate,capital, code2])    
+    return response
 
-
+###
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
         # display links to 3 options (country / region / continent)
         return render_template("index.html")
     # retrieve data from the database based on the selected option and present it to the user
-    raise NotImplementedError
+    else:
+        if request.form.get("country"):
+            country = request.form.get("country")
+            if country in CACHE:
+                result = CACHE[country]
+            else:
+                result = get_country_info("name",country)
+                CACHE[country] = result
+            return render_template("index.html", rows=result)
+        if request.form.get("continent"):
+            continent = request.form.get("continent")
+            if continent in CACHE:
+                result = CACHE[continent]
+            else:
+                result = get_country_info("continent", continent)
+                CACHE[continent] = result
+            return render_template("index.html", rows=result)
+        if request.form.get("region"):
+            region = request.form.get("region")
+            if region in CACHE:
+                result = CACHE[continent]
+            else:
+                result = get_country_info("region", region)
+                CACHE[region] = result
+            return render_template("index.html", rows=result)
+                
 
 
 @app.route("/<string:scope>")
 def search(scope: str):
+    global THE_WORLD
     if scope == "country":
         # get countries from the database and populate options of the drop-down menu
-        raise NotImplementedError
+        THE_WORLD = get_data_from_db(
+            query= f" select name from country order by name asc"
+        )
+        return render_template("country.html", options=THE_WORLD)
+
     elif scope == "region":
         # get regions from the database and populate options of the drop-down menu
-        raise NotImplementedError
+        THE_WORLD = get_data_from_db(
+            query= f" select region from country order by country.region asc"
+        )
+        return render_template("region.html", options=THE_WORLD)
     elif scope == "continent":
         # get continents from the database and populate options of the drop-down menu
-        raise NotImplementedError
+        THE_WORLD = get_data_from_db(
+            query= f" select continent from country order by country.continent asc"
+        )
+        return render_template("continent.html", options=THE_WORLD)
