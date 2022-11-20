26863f2a1643cc0d2fad51455a9bea42b45204fb
Phat Tu <77705393+sturekev@users.noreply.github.com>
2022-11-20 13:21:20 -0600

    store done

--- projects/store/app.py
+++ projects/store/app.py
@@ -4,21 +4,21 @@ from models import Item, ItemSchema
 @app.route("/")
 def index():
     return render_template("index.html")
-@app.route("/<string:scope>", methods = ["GET", "POST"])
-def accessToInventory(scope: str):
-    if scope == "list":
-        items = Item.query.all()
-        item_schema = ItemSchema(many=True)        
-        return render_template("list.html", items=item_schema.dump(items))
-    elif scope == "add":
-        if request.method == "POST" :
-            new_item = Item(
-                name=request.form.get("item_name"),
-                category=request.form.get("item_category"),
-                price=int(request.form.get('item_price'))
-            )
-            db.session.add(new_item)
-            db.session.commit()
-            # message=f"The item {request.form.get('item_name')} has been successfully added"
-            return render_template('add.html')
-        return render_template('add.html', message=None)
+@app.route("/list")
+def show_list():
+    items = Item.query.all()
+    item_schema = ItemSchema(many=True)
+    return render_template("list.html", items=item_schema.dump(items))
+
+@app.route("/add", methods=["GET", "POST"])
+def add_item():
+    if request.method == "POST" :
+        new_item = Item(
+            name=request.form.get("item_name"),
+            category=request.form.get("item_category"),
+            price=request.form.get('item_price')
+        )
+        db.session.add(new_item)
+        db.session.commit()
+        return render_template('add.html', message=f"The item {request.form.get('item_name')} has been successfully added")
+    return render_template('add.html', message=None)
